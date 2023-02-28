2cd437495dd42ecf43812264810ff49f23049fa4
Katie Sandfort <katie.sandfort@gmail.com>
2023-02-28 12:03:18 -0800

    successfully rendered first react component to electron app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                Hi there!
+            </h1>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root') 
+)
